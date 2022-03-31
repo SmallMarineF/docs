50eab94ae89cd59e44d95d69b31a0e27103924e8
mankueng <mankueng1011@gmail.com>
2022-03-31 14:31:21 +0800

    fix: 站点图片逻辑修改

---
+++ docs/assets/file.html.46a634a0.js
@@ -0,0 +1,66 @@
+import{_ as e,g as a,o as r,c as l,h as b,w as i,F as c,b as n,i as t}from"./app.02eae836.js";const m={},p=n("h1",{id:"\u76EE\u5F55\u7ED3\u6784",tabindex:"-1"},[n("a",{class:"header-anchor",href:"#\u76EE\u5F55\u7ED3\u6784","aria-hidden":"true"},"#"),t(" \u76EE\u5F55\u7ED3\u6784")],-1),u=n("div",{class:"custom-container tip"},[n("p",{class:"custom-container-title"},"\u63D0\u793A"),n("p",null,"\u76EE\u5F55\u7ED3\u6784\u4E00\u822C\u662F\u56E2\u961F\u89C4\u5B9A\uFF0C\u8FD9\u91CC\u4E0D\u505A\u7EDF\u4E00\u8981\u6C42\u3002")],-1),o=n("div",{class:"language-markdown ext-md line-numbers-mode"},[n("pre",{class:"language-markdown"},[n("code",null,`vite_vue_ts // \u9879\u76EE\u6839
+\u251C\u2500\u2500 .husky // git\u89C4\u8303
+\u2502   \u251C\u2500\u2500 _
+\u2502   \u251C\u2500\u2500 commit-msg
+\u2502   \u2514\u2500\u2500 pre-commit
+\u251C\u2500\u2500 .vscode // ide\u8BBE\u7F6E
+\u2502   \u251C\u2500\u2500 extensions.json
+\u2502   \u2514\u2500\u2500 settings.json
+\u251C\u2500\u2500 config // vite\u914D\u7F6E
+\u2502   \u251C\u2500\u2500 plugins
+\u2502   \u251C\u2500\u2500 build.ts
+\u2502   \u251C\u2500\u2500 css.ts
+\u2502   \u251C\u2500\u2500 index.ts
+\u2502   \u251C\u2500\u2500 resolve.ts
+\u2502   \u2514\u2500\u2500 server.ts
+\u251C\u2500\u2500 mock // \u6A21\u62DF\u6570\u636E
+\u2502   \u2514\u2500\u2500 home.ts
+\u251C\u2500\u2500 public // \u9759\u6001\u76EE\u5F55
+\u2502   \u2514\u2500\u2500 favicon.ico
+\u251C\u2500\u2500 src // \u6E90\u7801
+\u2502   \u251C\u2500\u2500 assets
+\u2502   \u251C\u2500\u2500 components
+\u2502   \u251C\u2500\u2500 config
+\u2502   \u251C\u2500\u2500 directives
+\u2502   \u251C\u2500\u2500 global
+\u2502   \u251C\u2500\u2500 http
+\u2502   \u251C\u2500\u2500 icons
+\u2502   \u251C\u2500\u2500 lang
+\u2502   \u251C\u2500\u2500 mixins
+\u2502   \u251C\u2500\u2500 pages
+\u2502   \u251C\u2500\u2500 routers
+\u2502   \u251C\u2500\u2500 store
+\u2502   \u251C\u2500\u2500 style
+\u2502   \u251C\u2500\u2500 utils
+\u2502   \u251C\u2500\u2500 vuex
+\u2502   \u251C\u2500\u2500 App.vue
+\u2502   \u251C\u2500\u2500 env.d.ts
+\u2502   \u251C\u2500\u2500 index.d.ts
+\u2502   \u2514\u2500\u2500 main.ts
+\u251C\u2500\u2500 templates // \u6A21\u677F\u751F\u6210
+\u2502   \u251C\u2500\u2500 component
+\u2502   \u251C\u2500\u2500 store
+\u2502   \u2514\u2500\u2500 utils.js
+\u251C\u2500\u2500 types // ts\u7C7B\u578B
+\u2502   \u251C\u2500\u2500 auto-imports.d.ts
+\u2502   \u2514\u2500\u2500 components.d.ts
+\u251C\u2500\u2500 .editorconfig // \u7F16\u7801\u89C4\u8303
+\u251C\u2500\u2500 .env.dev // \u73AF\u5883\u8BBE\u7F6E
+\u251C\u2500\u2500 .env.prod
+\u251C\u2500\u2500 .env.test
+\u251C\u2500\u2500 .eslintignore
+\u251C\u2500\u2500 .eslintrc.js // eslint\u8BBE\u7F6E
+\u251C\u2500\u2500 .prettierignore
+\u251C\u2500\u2500 .prettierrc.js // \u98CE\u683C\u8BBE\u7F6E
+\u251C\u2500\u2500 .stylelintignore
+\u251C\u2500\u2500 .stylelintrc.js // \u6837\u5F0F\u7EDF\u4E00
+\u251C\u2500\u2500 README.md //\u8BF4\u660E\u95EE\u7B54
+\u251C\u2500\u2500 commitlint.config.js // \u914D\u7F6E\u4FE1\u606F
+\u251C\u2500\u2500 index.html
+\u251C\u2500\u2500 package.json // \u5305\u7BA1\u7406
+\u251C\u2500\u2500 plopfile.js
+\u251C\u2500\u2500 pnpm-lock.yaml
+\u251C\u2500\u2500 tsconfig.json
+\u251C\u2500\u2500 tsconfig.node.json
+\u2514\u2500\u2500 vite.config.ts
+`)]),n("div",{class:"line-numbers","aria-hidden":"true"},[n("span",{class:"line-number"},"1"),n("br"),n("span",{class:"line-number"},"2"),n("br"),n("span",{class:"line-number"},"3"),n("br"),n("span",{class:"line-number"},"4"),n("br"),n("span",{class:"line-number"},"5"),n("br"),n("span",{class:"line-number"},"6"),n("br"),n("span",{class:"line-number"},"7"),n("br"),n("span",{class:"line-number"},"8"),n("br"),n("span",{class:"line-number"},"9"),n("br"),n("span",{class:"line-number"},"10"),n("br"),n("span",{class:"line-number"},"11"),n("br"),n("span",{class:"line-number"},"12"),n("br"),n("span",{class:"line-number"},"13"),n("br"),n("span",{class:"line-number"},"14"),n("br"),n("span",{class:"line-number"},"15"),n("br"),n("span",{class:"line-number"},"16"),n("br"),n("span",{class:"line-number"},"17"),n("br"),n("span",{class:"line-number"},"18"),n("br"),n("span",{class:"line-number"},"19"),n("br"),n("span",{class:"line-number"},"20"),n("br"),n("span",{class:"line-number"},"21"),n("br"),n("span",{class:"line-number"},"22"),n("br"),n("span",{class:"line-number"},"23"),n("br"),n("span",{class:"line-number"},"24"),n("br"),n("span",{class:"line-number"},"25"),n("br"),n("span",{class:"line-number"},"26"),n("br"),n("span",{class:"line-number"},"27"),n("br"),n("span",{class:"line-number"},"28"),n("br"),n("span",{class:"line-number"},"29"),n("br"),n("span",{class:"line-number"},"30"),n("br"),n("span",{class:"line-number"},"31"),n("br"),n("span",{class:"line-number"},"32"),n("br"),n("span",{class:"line-number"},"33"),n("br"),n("span",{class:"line-number"},"34"),n("br"),n("span",{class:"line-number"},"35"),n("br"),n("span",{class:"line-number"},"36"),n("br"),n("span",{class:"line-number"},"37"),n("br"),n("span",{class:"line-number"},"38"),n("br"),n("span",{class:"line-number"},"39"),n("br"),n("span",{class:"line-number"},"40"),n("br"),n("span",{class:"line-number"},"41"),n("br"),n("span",{class:"line-number"},"42"),n("br"),n("span",{class:"line-number"},"43"),n("br"),n("span",{class:"line-number"},"44"),n("br"),n("span",{class:"line-number"},"45"),n("br"),n("span",{class:"line-number"},"46"),n("br"),n("span",{class:"line-number"},"47"),n("br"),n("span",{class:"line-number"},"48"),n("br"),n("span",{class:"line-number"},"49"),n("br"),n("span",{class:"line-number"},"50"),n("br"),n("span",{class:"line-number"},"51"),n("br"),n("span",{class:"line-number"},"52"),n("br"),n("span",{class:"line-number"},"53"),n("br"),n("span",{class:"line-number"},"54"),n("br"),n("span",{class:"line-number"},"55"),n("br"),n("span",{class:"line-number"},"56"),n("br"),n("span",{class:"line-number"},"57"),n("br"),n("span",{class:"line-number"},"58"),n("br"),n("span",{class:"line-number"},"59"),n("br"),n("span",{class:"line-number"},"60"),n("br"),n("span",{class:"line-number"},"61"),n("br"),n("span",{class:"line-number"},"62"),n("br"),n("span",{class:"line-number"},"63"),n("br"),n("span",{class:"line-number"},"64"),n("br"),n("span",{class:"line-number"},"65"),n("br")])],-1);function d(g,_){const s=a("code-type");return r(),l(c,null,[p,u,b(s,{title:"\u793A\u4F8B"},{default:i(()=>[o]),_:1})],64)}var f=e(m,[["render",d]]);export{f as default};
--- docs/assets/index.html.da35093f.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./app.b60d3074.js";const e={};function _(n,t){return null}var a=r(e,[["render",_]]);export{a as default};
